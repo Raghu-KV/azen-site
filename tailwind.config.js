2b1b3fa6506db51e82a8f82a468a64d84538e148
Raghu-KV <raghunandanv19@gmail.com>
2023-11-11 21:40:51 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        pirmary: "#8339e4",
+        secondery: "#fe759b",
+        extra: "#3bd6f4",
+      },
+    },
+  },
+  plugins: [],
+};

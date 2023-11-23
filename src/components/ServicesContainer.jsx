3b104ef8e19406e56ffd22862e8b3155742d0474
Raghu-KV <raghunandanv19@gmail.com>
2023-11-23 20:08:28 +0530

    image

--- src/components/ServicesContainer.jsx
+++ src/components/ServicesContainer.jsx
@@ -1,4 +1,7 @@
 import image from "../asset/How-to-Build-Your-Own-Laptop-From-Scratch-removebg-preview.png";
+import e_commerce from "../asset/e-commerce.png";
+import soft from "../asset/software.png";
+import ui from "../asset/ui.png";
 import SingleServiceContainer from "./SingleServiceContainer";
 
 const dev = [
@@ -12,19 +15,19 @@ const dev = [
     heading: "E-Commerce Solutions",
     content:
       "Ready to launch your online store? We design and develop secure, scalable, and feature-rich e-commerce websites that drive sales and provide a smooth shopping experience for your customers. From payment gateways to inventory management, we've got you covered",
-    image: image,
+    image: e_commerce,
   },
   {
     heading: "Custom Software Development",
     content:
       "Your business is unique, and so are its needs. Our custom software solutions are tailored to address your specific requirements, optimizing processes and enhancing overall efficiency. We work closely with you to understand your goals and deliver a solution that exceeds expectations.",
-    image: image,
+    image: soft,
   },
   {
     heading: "User Experience (UX) & User Interface (UI) Design",
     content:
       " We understand that the success of any digital product hinges on how well it engages and delights its users. With a passionate team of UX/UI designers, we are dedicated to creating seamless, visually stunning, and user-centric interfaces that leave a lasting impression.",
-    image: image,
+    image: ui,
   },
 ];
 

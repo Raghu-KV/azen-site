1cc5f4e6491aaa3831ae6d1c17fafc60103b61fe
Raghu-KV <raghunandanv19@gmail.com>
2023-11-27 19:14:17 +0530

    hg

--- src/components/FeedBack.jsx
+++ src/components/FeedBack.jsx
@@ -21,9 +21,9 @@ function FeedBack() {
   ];
 
   return (
-    <div className="px-10 mx-auto bg-gradient-to-r from-purple-800 to-pirmary">
+    <div className="px-10 mx-auto bg-gradient-to-tl from-white to-gray-200">
       <div className="container  font-poppins mx-auto rounded-2xl pb-10 lg:p-10">
-        <h3 className="font-poppins font-bold text-center text-2xl text-white lg:text-3xl px-2 py-5 mb-4">
+        <h3 className="font-poppins font-bold text-center text-2xl text-black lg:text-3xl px-2 py-5 mb-4">
           Yes, We Deliver on the Promise !
         </h3>
         <div className="flex flex-col gap-6 mb-10 lg:flex-row lg:px-10">
--- src/components/SingleFeedBack.jsx
+++ src/components/SingleFeedBack.jsx
@@ -2,20 +2,22 @@ import { ImQuotesLeft } from "react-icons/im";
 
 function SingleFeedBack({ feedBack }) {
   return (
-    <div className="border-white border px-5 py-5 text-white  rounded-2xl basis-1/3">
+    <div className="border-black border px-5 py-5 text-white  rounded-2xl basis-1/3">
       <div>
-        <ImQuotesLeft size={45} className="text-secondery" />
+        <ImQuotesLeft size={45} className="text-pirmary" />
       </div>
       <div className="flex">
         <div className="flex flex-col">
-          <p className="text-sm text-justify text-gray-200 mt-5">
+          <p className="text-sm text-justify text-gray-500 mt-5">
             {feedBack.content}
           </p>
           <div className="flex items-center mt-3 gap-3">
-            <div className="w-12 h-12 bg-slate-400 rounded-full"></div>
+            <div className="px-4 py-2 bg-gray-500 rounded-full font-poppins font-bold text-center text-xl">
+              {feedBack.person[0]}
+            </div>
             <div>
-              <h3 className=" font-semibold">{feedBack.person}</h3>
-              <h4 className="text-sm">{feedBack.postion}</h4>
+              <h3 className="text-pirmary font-semibold">{feedBack.person}</h3>
+              <h4 className="text-sm text-gray-500">{feedBack.postion}</h4>
             </div>
           </div>
         </div>
